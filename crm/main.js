b7a053aacaa6c6304ea1042d8cae052d67536f7e
TataFart <tata.fartukova@yandex.ru>
2024-04-25 21:18:36 +0300

    first js

---
+++ crm/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const product = "Коварный лис";
+
+const category = "Настольные игры";
+
+const price = 1200;
+
+const amount = 3;
+
+const sum = price * amount;
+
+console.log(sum)
+
+
+
