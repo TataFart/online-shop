fb3a418519fede8a5b1c852b8dbba853c1d191c0
TataFart <tata.fartukova@yandex.ru>
2024-04-26 10:12:16 +0300

    lesson1

--- crm/main.js
+++ crm/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const product = "Коварный лис";
-
+console.log("Название продукта:" , product);
 const category = "Настольные игры";
-
+console.log("Категория:" ,category)
 const price = 1200;
-
+console.log("Цена: ", price);
 const amount = 3;
+console.log("Количество: ", amount);
 
-const sum = price * amount;
-
-console.log(sum)
+console.log("Общая сумма: ", price * amount)
 
 
 
---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const product = "Коварный лис";
+console.log("Название продукта:" , product);
+const category = "Настольные игры";
+console.log("Категория:" ,category)
+const price = 1200;
+console.log("Цена: ", price);
+const amount = 3;
+console.log("Количество: ", amount);
+
+console.log("Общая сумма: ", price * amount)
